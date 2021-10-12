b9f824c11fb60c7ad0570923ce5bc425d68ae92f
Yaroslav.Buiukli <yarik20333@gmail.com>
2021-10-12 19:50:25 +0300

    added new test

---
+++ Test_2_titles/TitlesTest.js
@@ -0,0 +1,15 @@
+const {Builder,By,Key} = require("selenium-webdriver");
+
+async function title_test (){
+    let driver = await new Builder().forBrowser('chrome').build();
+    await driver.get('http://automationpractice.com/index.php');
+
+    let title = await driver.getTitle();
+
+    if (title === 'My Store') console.log('\n Passed \n');
+    else console.log('\n Failed \n');
+
+    await driver.quit();
+}
+
+title_test();
