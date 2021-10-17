3f79ff1bb2c6fcb46f434f893b59dcb78cc28772
Yaroslav.Buiukli <yarik20333@gmail.com>
2021-10-17 20:01:50 +0300

    update to try and catch construction

--- Test_2_titles/TitlesTest.js
+++ Test_2_titles/TitlesTest.js
@@ -2,14 +2,21 @@ const {Builder,By,Key} = require("selenium-webdriver");
 
 async function title_test(){
     let driver = await new Builder().forBrowser('chrome').build();
-    await driver.get('http://automationpractice.com/index.php');
 
-    let title = await driver.getTitle();
+    try{
+        await driver.get('http://automationpractice.com/index.php');
 
-    if (title === 'My Store') console.log('\n Passed \n');
-    else console.log('\n Failed \n');
+        let title = await driver.getTitle();
 
-    await driver.quit();
+        if (title === 'My Store') console.log('\n Passed \n');
+        else console.log('\n Failed \n');
+    }
+    catch(e){
+        if (e.name === 'NoSuchElementError') console.log('Element not found \n Failed');
+    }
+    finally{
+        await driver.quit();
+    }
 }
 
 title_test();
